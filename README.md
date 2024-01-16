0b2738dcb6d144b10ac1da8f6a52d49372303991
calicxy <calista.choy@yahoo.com>
2024-01-16 17:12:34 +0800

    3a

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # htx-tech-test
+*Note: The codes and commands are run on a Windows laptop.
+
+To run Flask app asr_api:
+1. python3 ./asr/asr_api.py
+
+To run the Docker container for the asr_api app:
+1. cd asr
+2. docker build --tag asr-api .
+3. docker run
---
+++ asr-train/evaluate_transcribed.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import zipfile
+import os
+from datasets import load_metric
+
+wer_metric = load_metric("wer")
+
+df = pd.read_csv("asr-train/cv-valid-dev_transcribed.csv")
+# print(df.head())
+
+predictions = df["generated_text"].apply(lambda x: str(x).lower()).values
+reference = df["text"].values
+    
+print(wer_metric.compute(predictions=predictions, references=reference))
+# WER = 0.10825
+
+ft_df = pd.read_csv("asr-train/cv-valid-dev_finetuned.csv")
+
+predictions = ft_df["generated_text"].apply(lambda x: str(x).lower()).values
+reference = ft_df["text"].apply(lambda x: str(x).lower()).values
+
+print(wer_metric.compute(predictions=predictions, references=reference))
+# WER = 0.10596
